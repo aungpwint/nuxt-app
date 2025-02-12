998b0968458d25714e8180d9ee449676ddd0ff1e
Aung Pwint <aungpwintofficial@gmail.com>
2025-02-12 12:07:40 +0630

    app config

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,13 @@
-declare module 'nuxt/schema' {}
+declare module 'nuxt/schema' {
+    interface AppConfigInput {
+        /** Sidebar configuration */
+        sidebar?: {
+            collapsible?: 'offcanvas' | 'icon' | 'none'
+            side?: 'left' | 'right'
+            variant?: 'sidebar' | 'floating' | 'inset'
+        }
+    }
+}
 
 // It is always important to ensure you import/export something when augmenting a type
 export {}
