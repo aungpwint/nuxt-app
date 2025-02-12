0370daea63753c30afb4a9a69cee41ab4df5f16d
Aung Pwint <aungpwintofficial@gmail.com>
2025-02-12 12:26:03 +0630

    store and vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,15 @@ const currentDir = dirname(fileURLToPath(import.meta.url))
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@unocss/nuxt', 'shadcn-nuxt', '@nuxtjs/color-mode', '@nuxt/icon', '@nuxt/eslint'],
+    modules: [
+        '@unocss/nuxt',
+        'shadcn-nuxt',
+        '@nuxtjs/color-mode',
+        '@nuxt/icon',
+        '@pinia/nuxt',
+        '@vueuse/nuxt',
+        '@nuxt/eslint',
+    ],
 
     components: [
         {
