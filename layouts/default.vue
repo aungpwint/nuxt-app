1bae9edcf304efdf55354ebaf148f9820922483d
Aung Pwint <aungpwintofficial@gmail.com>
2025-02-12 12:52:50 +0630

    ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,6 @@
 
 <template>
     <div>
-        <h1>Welcome</h1>
+        <slot />
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Button } from '@/components/ui/button'
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select'
+</script>
 
 <template>
-    <div>
-        <h1>Welcome</h1>
-    </div>
+    <Card class="w-[350px]">
+        <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>Deploy your new project in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent> </CardContent>
+        <CardFooter class="flex justify-between px-6 pb-6">
+            <Button variant="outline"> Cancel </Button>
+            <Button>Deploy</Button>
+        </CardFooter>
+    </Card>
 </template>
